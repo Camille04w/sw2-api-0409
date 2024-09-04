8d063568590dcac8b82a29115b7068525375ecfd
Camille04w <vitoriacamille603@gmail.com>
2024-09-04 13:29:49 +0000

    save2

--- app/src/main/java/application/controller/TesteController.java
+++ app/src/main/java/application/controller/TesteController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/frutas")
 public class TesteController { 
-    private String[] f = {"Maça", "Banana", "Morango"};  
+    private String[] f = {"Maçã", "Banana", "Morango"};  
     @GetMapping // usado por causa do métoto get
     public Iterable<String> get() { // convertendo o vetor em um <iterable>, e retorna um arquivo json
         return Arrays.asList(f);
